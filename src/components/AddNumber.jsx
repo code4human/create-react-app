2f84965c03fbb6d06f500ded3017fba353a602fd
TenaKim <tena.kim10@gmail.com>
2020-08-28 22:05:19 +0900

    give and take values with only react props

--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 
 export default class AddNumber extends Component{
+    state = {size:1}
     render(){
       return (
         <div>
           <h1>Add Number</h1>
-          <input type="button" value="+"></input>
-          <input type="text" value="0"></input>
+          <input type="button" value="+" onClick={function(){
+              this.props.onClick(this.state.size);
+          }.bind(this)}></input>
+          <input type="text" value={this.state.size} onChange={function(event){
+              this.setState({size:Number(event.target.value)});
+          }.bind(this)}></input>
         </div>
       )
     }
