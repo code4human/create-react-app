9f130a1bf06fbb37d3389332ab422633c237789f
TenaKim <tena.kim10@gmail.com>
2020-08-28 14:31:18 +0900

    basic custom

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+
+class AddNumberRoot extends Component{
+  render(){
+    return (
+      <div>
+        <h1>Add Number Root</h1>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
     </div>
   );
 }
